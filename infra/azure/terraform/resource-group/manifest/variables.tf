caa926ee4bf2e1910cb89d827d2fcdb23233a92c
Ieso Dias <iesodias@gmail.com>
2023-01-18 18:53:30 -0300

    automated commit

---
+++ infra/azure/terraform/resource-group/manifest/variables.tf
@@ -0,0 +1,11 @@
+variable "azure_region" {
+  default = "eastus"
+  description = "Azure Region where resources to be created"
+  type = string
+}
+
+variable "resource_group_name" {
+  default = "my-demo-rg1"
+  description = "Azure Region where resources to be created"
+  type = string
+}
