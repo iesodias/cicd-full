6b201994209934f5edfbe7181a576db2225dbca0
Ieso Dias <iesodias@gmail.com>
2023-01-18 17:56:58 -0300

    automated commit

---
+++ app/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, make_response, Request
+from flask_restful import Resource, Api
+import socket, os
+
+app = Flask(__name__, template_folder='templates_folder')
+
+@app.route("/goku")
+def goku():
+  return render_template('goku.html')
+
+@app.route("/host")
+def host():
+  return render_template('host.html', id_container=socket.gethostname())
+
+@app.route("/success", methods=["GET"])
+def success():
+    response = make_response("<h1>Success</h1>", 200)
+    return response
+
+@app.route("/denied", methods=["GET"])
+def denied():
+    response = make_response("<h1>Denied</h1>", 500)
+    return response
+
+@app.route("/notfound", methods=["GET"])
+def notfound():
+    response = make_response("<h1>NotFound</h1>", 404)
+    return response
+
+if __name__ == "__main__":
+ port = int(os.environ.get('PORT', 5000))
+ app.run(debug=True, host='0.0.0.0', port=port)
+
+
+
+
+
+# @app.route("/userDetails", methods=["GET", "POST"])
+# def user_details():
+
+#     if request.method == "POST":
+        
+#         username = request.form.get("username")
+#         firstname = request.form.get("firstname")
+#         lastname = request.form["lastname"]
+
+#         response = make_response("<h1>Success</h1>", 201)
+#         return response
+
+# @app.route("/userSignUp", methods=["POST"])
+# def sign_up():
+
+#     if request.method == "POST":
+
+#         username = request.form.get("username")
+#         password = request.form.get("password")
+        
+#         response = make_response("<h1>Success</h1>")
+#         response.status_code = 200
+#         return response
